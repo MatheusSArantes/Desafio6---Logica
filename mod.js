5707b47f072b182f83c33a9a2c034e802edcbbb4
“MatheusSArantes” <matheus.sodre.arantes@gmail.com>
2021-05-28 10:57:08 -0300

    Desafio6

---
+++ mod.js
@@ -0,0 +1,45 @@
+let pessoas = {
+    nome: "Leviii",
+    idade: 17,
+    amigos: ["Amigo1", "Amigo2", "Amigo3"]
+  }
+  console.log(pessoas)
+  console.log(pessoas.nome.length)
+
+var cadastro = [
+    {
+        nome: "Leviii",
+        idade: 21,
+        telefone:9238364830,
+        amigos: ["Amigo1", "Amigo2", "Amigo3"],
+    },
+    {
+        nome: "Kenyyy",
+        idade: 60,
+        telefone:9453462624,
+        amigos: ["Amigo1", "Amigo2", "Amigo3"],
+    },
+    {
+        nome: "Eren",
+        idade: 18,
+        telefone:9735869938,
+        amigos: ["Amigo1", "Amigo2", "Amigo3"],
+    },
+    {
+        nome: "Mikasa",
+        idade: 18,
+        telefone:9573469479,
+        amigos: ["Amigo1", "Amigo2", "Amigo3"],
+    },
+    {
+        nome: "Jean",
+        idade: 19,
+        telefone:9749580700,
+        amigos: ["Amigo1", "Amigo2", "Amigo3"],
+    },
+]
+console.log(cadastro[0].amigos[0])
+console.log(cadastro[1].amigos[0])
+console.log(cadastro[2].amigos[0])
+console.log(cadastro[3].amigos[0])
+console.log(cadastro[4].amigos[0])
